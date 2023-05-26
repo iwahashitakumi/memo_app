666314d77b9c1b0584ad44ee070fc04c149c9b15
EC2 Default User <ec2-user@ip-172-31-34-233.ap-southeast-2.compute.internal>
2023-05-26 19:38:32 +0000

    メモ課題

--- memo.rb
+++ memo.rb
@@ -1,4 +1,4 @@
-require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
+require "csv" 
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
 puts "1か2を入力してください"
@@ -12,7 +12,8 @@ if memo_type == 1
  file_name = gets.chomp
  puts "メモの内容を記入してください"
  puts "完了したらCtrl + Dを押します"
- memo = STDIN.read
+ input_memo = STDIN.read
+ memo = input_memo.chomp
  
  csv = CSV.open("#{file_name}.csv" , "w") 
  csv.puts ["#{memo}"]
@@ -22,10 +23,9 @@ elsif memo_type == 2
  puts "既存のファイルを編集します"
  puts "拡張子を除いた既存のファイルを入力してください"
   file_name = gets.chomp
-  puts "メモの内容を記入してください"
-　puts "完了したらCtrl + Dを押します"
-  memo = STDIN.read
-　
+ puts "メモの内容を記入してください"
+ puts "完了したらCtrl+Dを押します"
+ memo = STDIN.read
 　csv = CSV.open("#{file_name}.csv", "a") 
 　csv.puts ["#{memo}"]
 　csv.close
